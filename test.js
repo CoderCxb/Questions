8b753eb669fa570792bebc260856287605cd4f81
cxblovecw <873587091@qq.com>
2021-01-05 00:43:01 +0800

    'new'

--- test.js
+++ test.js
@@ -1,19 +1,2 @@
-function objectFactory() {
-    var obj = new Object()
-
-    var Constructor = [].shift.call(arguments);
-
-    obj.__proto__ = Constructor.prototype;
-
-    var ret = Constructor.apply(obj, arguments);
-
-    return typeof ret === 'object' ? ret : obj;
-};
-
-function test(){
-    console.log(this);
-}
-
-
-
-// objectFactory(test)
+console.log(a);
+var a=10;
