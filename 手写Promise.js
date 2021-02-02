ff59000955b810d7e4550fd82b835d710de9c975
cxblovecw <873587091@qq.com>
2021-02-02 16:05:54 +0800

    update

--- 手写Promise.js
+++ 手写Promise.js
@@ -48,101 +48,117 @@ const resolvePromise = (promise2, x, resolve, reject) => {
 		resolve(x);
 	}
 };
+
+// 异步resolve时 先执行then中的pending添加回调 再执行resolve
+// 同步resolve时 直接执行resolve 此时数组为空 所以没什么太大意义， 然后执行then中的fulfilled 直接执行传入的函数
 class MyPromise {
-	status = PENDING;
-	value = undefined;
-	reason = undefined;
-	onResolvedCallback = [];
-	onRejectedCallback = [];
 	constructor(executor) {
+		this.status = PENDING; // 状态
+		this.value = undefined; // 成功传递的值
+		this.reason = undefined; // 失败的原因
+		// 使用数组是因为当resolve在异步操作中时 then中获取不到
+		this.onResolvedCallback = []; // 成功的回调函数数组
+		this.onRejectedCallback = []; // 失败的回调函数数组
+		// 将状态更改为完成 fulfilled 并执行成功的回调函数
+		// 当resolve是异步调用时 会先执行所有then中的pending添加回调函数 再执行resolve 遍历执行回调 而回调中会执行返回的promise的resolve 因此会连续触发 将所有的resolve都执行完毕
+		// 如果resolve是同步的 则执行then中fulfilled情况 直接执行resolve
 		let resolve = (value) => {
-			this.status = FULFILLED;
-			this.value = value;
-			this.onResolvedCallback.forEach((fn) => fn(this.value));
+			if (this.status === PENDING) {
+				this.status = FULFILLED;
+				this.value = value;
+				this.onResolvedCallback.forEach((fn) => fn());
+			}
 		};
+		// 将状态更改为异常 reject 并执行失败的回调函数
 		let reject = (reason) => {
 			if (this.status === PENDING) {
 				this.reason = reason;
 				this.status = REJECTED;
-				this.onRejectedCallback.forEach((fn) => fn(this.reason));
+				this.onRejectedCallback.forEach((fn) => fn());
 			}
 		};
+		// 执行传递进来的函数 若发生异常 则调用reject
 		try {
 			executor(resolve, reject);
 		} catch (err) {
 			reject(err);
 		}
 	}
+	// 接收两个参数 成功的回调和失败的回调
 	then(onFulfilled, onRejected) {
+		// 防止then中没有回调函数 (v)=>v会将value返回给下一个then
 		onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : (v) => v;
+		// 防止catch没有回调函数 err=>throw err抛出异常
 		onRejected =
 			typeof onRejected === 'function'
 				? onRejected
 				: (err) => {
 						throw err;
 				  };
+		// then方法返回一个promise对象 为的是链式调用
 		let promise = new MyPromise((resolve, reject) => {
+			// 此处的FULFILLED和REJECTED判断是当resolve不是异步的时候 直接调用的
 			if (this.status === FULFILLED) {
-				// console.log(result);
-				// this.onResolvedCallback.push(resolve.bind(this, result));
-				// this.onResolvedCallback.forEach((fn) => fn(this.value));
 				setTimeout(() => {
-					let result = onFulfilled(this.value);
-					// console.log('promise:', promise);
-					// console.log('result:', result);
-					resolve(result);
-				});
+					try {
+						// 这个是执行then中的成功回调
+						let result = onFulfilled(this.value);
+						// 此处执行resolve 触发返回的promise的then方法或reject方法
+						resolvePromise(promise, result, resolve, reject);
+					} catch (err) {
+						reject(err);
+					}
+				}, 0);
 			}
 			if (this.status === REJECTED) {
-				// let result = onRejected(this.reason);
-				// console.log(result);
-				// this.onRejectedCallback.push(reject.bind(this, result));
-				// this.onRejectedCallback.forEach((fn) => fn(this.reason));
 				setTimeout(() => {
-					let result = reject(this.reason);
-					// resolve(result);
-				});
+					try {
+						let result = onRejected(this.reason);
+						resolvePromise(promise, result, resolve, reject);
+					} catch (err) {
+						reject(err);
+					}
+				}, 0);
 			}
+			// 当resolve是异步的时候 就会调用这 将回调存储
 			if (this.status === PENDING) {
 				this.onResolvedCallback.push(() => {
 					setTimeout(() => {
-						let result = onFulfilled(this.value);
-						resolve(result);
-					});
+						try {
+							let result = onFulfilled(this.value);
+							resolvePromise(promise, result, resolve, reject);
+						} catch (err) {
+							reject(err);
+						}
+					}, 0);
 				});
 				this.onRejectedCallback.push(() => {
 					setTimeout(() => {
-						reject(this.reason);
-					});
+						try {
+							let result = onRejected(this.reason);
+							resolvePromise(promise, result, resolve, reject);
+						} catch (err) {
+							reject(err);
+						}
+					}, 0);
 				});
 			}
 		});
 		return promise;
 	}
-	catch(onReject) {
-		if (this.status === REJECTED) {
-			onReject(this.reason);
-		}
-	}
 }
 
 let p = new MyPromise(function (resolve, reject) {
-	// setTimeout(() => {
-	// console.log('test');
-	resolve('OK');
-	// }, 0);
+	setTimeout(() => {
+		resolve('OK');
+	}, 0);
 });
+
 p.then((data) => {
 	console.log(data);
-	return 'Nice';
+	return 'OK2';
 }).then((data) => {
 	console.log(data);
 });
-// .catch((err) => {
-// 	console.log('err:', err);
-// });
-// .then((data) => {
-// 	console.log(data);
-// });
+
 console.log('ASYNC');
-// let p3 = p2.then((value) => 100).then((value) => console.log(value));
