8654ae93b5fa0a70849ac4e5d014615a9875a7be
cxblovecw <873587091@qq.com>
2021-05-17 10:58:23 +0800

    update

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log(Object.prototype.toString.apply(1));
+class A {}
+class B {}
+
+let obj = {};
+obj.__proto__ = A.prototype;
+Object.setPrototypeOf(obj, new B());
+console.log(Object.getPrototypeOf(obj));
+console.log(obj instanceof A);
+// console.log(Object.getPrototypeOf(A));
