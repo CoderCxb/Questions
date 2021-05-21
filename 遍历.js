849bef6868840da74d9f4872ba41cfbcb6a0a616
cxblovecw <873587091@qq.com>
2021-05-21 18:08:53 +0800

    update

---
+++ 遍历.js
@@ -0,0 +1,31 @@
+async function test() {
+	let arr = [4, 2, 1];
+
+	// 无序,即便加了await,下一个异步也不会等上一个执行完毕再执行
+	arr.forEach(async (item) => {
+		const res = await handle(item);
+		console.log(res);
+	});
+
+	// 有序
+	// for (let index = 0; index < arr.length; index++) {
+	// 	const res = await handle(arr[index]);
+	// 	console.log(res);
+	// }
+	// 有序
+	// for (const iterator of arr) {
+	// 	const res = await handle(iterator);
+	// 	console.log(res);
+	// }
+	console.log('结束');
+}
+
+function handle(x) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(x);
+		}, 100 * x);
+	});
+}
+
+test();
