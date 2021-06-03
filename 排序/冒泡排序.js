17f6532463f294c81e70ef9523258d002c560e6e
cxblovecw <873587091@qq.com>
2021-06-04 01:18:39 +0800

    update

--- 排序/冒泡排序.js
+++ 排序/冒泡排序.js
@@ -1,13 +1,10 @@
 function sort(arr){
-    for(let i=0;i<arr.length-1;i++){
+    for(let i=0;i<arr.length;i++){
         for(let j=0;j<arr.length-1;j++){
-            if(arr[i]<arr[j]){
-                let num=arr[i];
-                arr[i]=arr[j];
-                arr[j]=num;
+            if(arr[j]>arr[j+1]){
+               [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
             }
         }
-        console.log(arr);
     }
     return arr;
 }
