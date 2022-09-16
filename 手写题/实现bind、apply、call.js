88fda3a65a4fd24822997005df16bb70277b8c19
chenxiaobin <873587091@qq.com>
2022-09-16 10:48:06 +0800

    fix: 修改bind写法

--- 手写题/实现bind、apply、call.js
+++ 手写题/实现bind、apply、call.js
@@ -9,24 +9,27 @@ Function.prototype.myCall = function (context, ...params) {
 // bind
 // 1.在Function的原型上添加属性
 // @ts-ignore
-Function.prototype.myBind = function (context, ...params1) {
+Function.prototype.myBind = function (_this, ...params1) {
   // 1.this是函数
   let self = this;
   // 2.context是接收的第一个参数 也就是this要指向的对象
   // params1则是传递给原函数的参数
-  context = context ? context : globalThis;
-  // 3.这里的this指的是需要改变指向的函数 将函数绑定到传入的对象上 这样调用方法的话 函数的this指的就是对象了
-  // 使用Symbol,避免重名等问题
-  let fn = Symbol("fn");
-  context.__proto__[fn] = self;
-  // 4.由于bind不是调用的 因此需要返回一个函数 用于外界触发
+  let context = _this ? _this : globalThis;
+  // 3.由于bind不是调用的 因此需要返回一个函数 用于外界触发
   let func = function (...params2) {
-    // 5.如果this是self的实例 说明使用了new关键字 因此返回this 否则不变
+    // 4.如果this是self的实例 说明使用了new关键字 因此返回this 否则不变
     // context=this instanceof self?this:context;
-    context = new.target ? this : context;
+    context = new.target ? this : _this;
+    // 5. Symbol避免重复
+    let fn = Symbol("fn");
+    // 6. 将self函数绑定到context进行调用
+    Object.defineProperty(context, fn, {
+      enumerable: false,
+      value: self
+    })
     return context[fn](...params1, ...params2);
   };
-  // 6. 让return的函数的原型和原函数保持一致
+  // 7. 让return的函数的原型和原函数保持一致
   func.prototype = self.prototype;
   return func;
 };
